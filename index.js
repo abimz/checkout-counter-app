c3aa6aa2e52f7f1c52cdf12e20714a2730b0d194
abimz <31736577+abimz@users.noreply.github.com>
2022-10-03 01:59:01 -0700

    Add files via upload

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
 //document.getElementById("count-el").innerText = 7
 
-let countEl = document.getElementById("count-el")
-let saveEl = document.getElementById("save-el")
+let countEl = document.getElementById("countItem-el")
+let saveEl = document.getElementById("checkout-el")
 
 let count = 0
 let prevNumber = count
 let resetCounter = 0
 
-function increment(){
+function addItem(){
     count += 1
     countEl.textContent = count
 }
 
-function save(){
+function checkout(){
     let countStr = count + " - "
     saveEl.textContent += countStr
     countEl.textContent = 0
